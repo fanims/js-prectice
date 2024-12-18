caa6c2a21dd1239806fb249fda96fcb3ff8551f3
fanims <farhanathar49@gmail.com>
2024-12-19 02:29:23 +0500

    js prectice

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-let star = "*";
-
-for (let i = 0; i < 10; i++) {
-    console.log(star);
-    document.getElementById("stars").innerHTML += star + "<br>";
-    star += "*";
-}   
-
-const age = 28;
-
-const result = age > 18 ? "Adult" : "Minor";
-
-document.getElementById("result").textContent = result;
+// Start learning JavaScript
+/*-----------
+    1- Variables
+    -- There are three types of variables
+     - var
+     - let
+     - const
+  -----------*/  
+let name = "Farhan Athar";
+const admin = name;
+alert(admin);
+/*-----------
+    1- Data types
+    -- There are seven types of variables
+     - String
+     - Numbers
+     - Boolean
+     - Arrays
+     - Objects
+     - Functions
+     - Null
+  -----------*/
+//   let name = "Ahmad";
+//   alert (`hello ${1}`);
+//   alert (`hello ${"name"}`);
+//   alert (`hello ${name}`);
